3fe912b383039fe5dfee2fbf00b0bee14a50bf65
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-21 22:48:45 +0300

    items

--- Перебор ключей и значений в словаре. Метод items/items/items_1.py
+++ Перебор ключей и значений в словаре. Метод items/items/items_1.py
@@ -1,20 +1,22 @@
 scores_dict = {
-    'Ваня': 33,
-    'Петя': 60,
-    'Лена': 45
+    'Петя': 60
 }
-
 for name in scores_dict.items():  # items
     print(f'Вывод кортежей: {name}')
-
 for name, scores in scores_dict.items():  # items
-    print(f'{name} : {scores}')
-
+    print(f'1 - {name} : {scores}')
 for name, scores in scores_dict.items():  # items
-    print(f'{name}')
-
+    print(f'2 - {name}')
 for name, scores in scores_dict.items():  # items
-    print(f'{scores}')
-
+    print(f'3 - {scores}')
 for name in scores_dict:
-    print(f'{name} : {scores_dict[name]}')
+    print(f'4 - {name} : {scores_dict[name]}')
+print('************************')
+
+score_dict = {
+    (5000, 123456): ('Иванов', 'Василий')
+}
+for name, scores in score_dict.items():  # items
+    print(f'1 - {name} : {scores}')
+for name, scores in score_dict.items():  # items
+    print(f'1 - {name[0]} {name[1]} : {scores[0]} {scores[1]}')
